0cd5ab9b415351a663b127a9fd387df6371e9bf4
Bruno Feliciano <104511327+Brunofell@users.noreply.github.com>
2023-09-11 15:11:04 -0300

    jtj

--- moments/src/app/components/moment-form/moment-form.component.ts
+++ moments/src/app/components/moment-form/moment-form.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { Moment } from 'src/app/Moment';
 
 @Component({
   selector: 'app-moment-form',
@@ -7,6 +9,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./moment-form.component.css']
 })
 export class MomentFormComponent {
+  @Output() onSubmit = new  EventEmitter<Moment>();
   @Input() btnText!: string;
 
   momentForm!: FormGroup;
@@ -31,6 +34,15 @@ export class MomentFormComponent {
     if(this.momentForm.invalid){
       return;
     }
-    console.log("Enviou Formulário!");
+    console.log(this.momentForm.value);
+    this.onSubmit.emit(this.momentForm.value);
   }
+
+  onFileSelected(event:any){
+    const file: File = event.target.files[0];
+
+    this.momentForm.patchValue({Image:file});
+  }
+
+
 }
--- moments/src/app/services/moment.service.ts
+++ moments/src/app/services/moment.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Moment } from '../Moment';
+import { environment } from 'src/environments/environment';
+import { formatDate } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MomentService {
 
-  constructor() { }
+  private baseApiURl = environment.baseApiUrl;
+  private apiUrl = '${this.baseApiURl}api/moments';
+
+  constructor( private http: HttpClient) {}
+
+    createMoment(formatDate: FormData): Observable<FormData>{
+        return  this.http.post<FormData>(this.apiUrl, formatDate)
+    }
+   
 }
